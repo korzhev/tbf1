de9faa291edebdae2208479e3bda006ca6c52aa6
vn.korzhev <vn.korzhev@tensor.ru>
2016-07-04 22:15:52 +0300

    first command

--- bot/db.js
+++ bot/db.js
@@ -15,6 +15,24 @@ class DB {
       res(true);
     });
   }
+
+  getDriverInfo(name, ru) {
+    console.log('infooo');
+    return new Promise((res, rej) => {
+      res({
+        name: 'Vettel',
+        stats: {
+          championships: 4,
+          wins: 40,
+          poles: 40,
+          fastlaps: 40,
+          seasons: 9,
+        },
+        bio: 'some big text',
+        photo: '/home/vk/Sebastian_Vettel_2011_Sebastian_Vettel_in_Yokohama_Infiniti_event.jpg',
+      });
+    });
+  }
 }
 
 module.exports = new DB();
--- bot/router.js
+++ bot/router.js
@@ -12,12 +12,29 @@ function router(bot) {
       try {
         const res = yield DB.getData();
         resp += res;
+        resp += fromId;
         console.info('yielded!');
       } catch (e) {
         console.info('error', e);
         resp += e;
       }
       bot.sendMessage(fromId, resp);
+    });
+  });
+
+  routeMap.set(/\/(pilot|driver|пилот|гонщик) (.+)/, (msg, match) => {
+    co(function* echo() {
+      const toId = msg.from.id;
+      const ru = match[1].charCodeAt(0) > 150;
+      let res = ru ? 'Гонщик не найден' : 'Pilot not found';
+      try {
+        res = yield DB.getDriverInfo(match[2], ru);
+        console.log(res);
+        const p = yield bot.sendPhoto(toId, 'AgADAgADrKcxG_BsdQ0dpIg3NStr-CMUSA0ABM3BX8tkay-7vhQBAAEC');
+      } catch (e) {
+        console.info('error', e);
+      }
+      bot.sendMessage(toId, JSON.stringify(res)).catch(console.error);
     });
   });
   return routeMap;
